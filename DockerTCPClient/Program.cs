fa372a129267e7b420891fcde20defe78ee07024
TGW <mc-casper@hotmail.dk>
2023-11-04 19:12:54 +0100

    Minor formatting fixes

--- DockerTCPClient/Program.cs
+++ DockerTCPClient/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Net.Sockets;
-TcpClient client = new TcpClient();
+TcpClient client = new();
 client.Connect("localhost", 12000);
 Console.WriteLine("Connected to server...");
 // Start a thread to receive messages
-Thread receiveThread = new Thread(() => ReceiveMessages(client));
+Thread receiveThread = new(() => ReceiveMessages(client));
 receiveThread.Start();
 // Send messages
-StreamWriter writer = new StreamWriter(client.GetStream());
+StreamWriter writer = new(client.GetStream());
 while (true)
 {
     string message = Console.ReadLine();
@@ -15,7 +15,7 @@ while (true)
 }
 void ReceiveMessages(TcpClient client)
 {
-    StreamReader reader = new StreamReader(client.GetStream());
+    StreamReader reader = new(client.GetStream());
     while (client.Connected)
     {
         string message = reader.ReadLine();
--- DockerTCPServer/Program.cs
+++ DockerTCPServer/Program.cs
@@ -1,22 +1,12 @@
-﻿//Console.WriteLine("Hello, World!");
-
-//var counter = 0;
-//var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-//while(max == -1 || counter < max)
-//{
-//    Console.WriteLine($"Counter: {++counter}");
-//    await Task.Delay(TimeSpan.FromMilliseconds(1_000));
-//}
-
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Net;
-TcpListener server = new TcpListener(IPAddress.Any, 12000);
+TcpListener server = new(IPAddress.Any, 12000);
 server.Start();
 Console.WriteLine("Server started... listening on port 12000");
 while (true)
 {
     TcpClient client = server.AcceptTcpClient();
-    Thread clientThread = new Thread(() => HandleClient(client));
+    Thread clientThread = new(() => HandleClient(client));
     clientThread.Start();
 }
 void HandleClient(TcpClient client)
@@ -24,10 +14,10 @@ void HandleClient(TcpClient client)
     Guid clientId = Guid.NewGuid();
     Console.WriteLine($"Client {clientId} connected.");
     // Send client name to the client
-    StreamWriter writer = new StreamWriter(client.GetStream());
+    StreamWriter writer = new(client.GetStream());
     writer.Flush();
     // Receive and send messages
-    StreamReader reader = new StreamReader(client.GetStream());
+    StreamReader reader = new(client.GetStream());
     try
     {
         while (client.Connected)
@@ -44,7 +34,7 @@ void HandleClient(TcpClient client)
     }
     catch (Exception ex)
     {
-        Console.WriteLine($"Exception occurred for client {clientId}: { ex.Message}");
+        Console.WriteLine($"Exception occurred for client {clientId}: {ex.Message}");
     }
     finally
     {
